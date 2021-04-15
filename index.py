2e6ef5409dba93c560b275163d0f24cbe0568cf3
goseungduk <a42873410@gmail.com>
2021-04-15 23:58:03 +0900

    210415_oneacc_multiThumbs_Scrolling

--- index.py
+++ index.py
@@ -6,27 +6,46 @@ import sys, os
 from bin import oneMulti
 from bin import fbLogin as FL # 임시 import 모듈 분리할 때까지만
 
+all_thumbs=[] # 전역 변수 자제해라
 def thumb_urls(driver): # 썸네일 url 따오기
+    global all_thumbs
     thumbs=driver.find_elements_by_css_selector('div.v1Nh3.kIKUG._bz0w')
     for thumb in thumbs:
-        link=thumb.find_elements_by_tag_name('a')
-        print(link[0].get_attribute('href'))
+        link=thumb.find_elements_by_tag_name('a')[0].get_attribute('href')
+        if(link not in all_thumbs):
+            all_thumbs.append(link)
 
 def oneacc_multi_thum_download(driver,url):
-    #aria-label="제어"
     collecting_urls(driver,url)
 
 def collecting_urls(driver,url):
+    global all_thumbs
     # https://www.instagram.com/dlwlrma/
     # https://www.instagram.com/accounts/signup/
     try:
         is_login_FB=driver.find_element_by_class_name('KPnG0')
     except: # 익명으로 로그인이 무사히 되었을 때
-        # 스크롤 작업 추가 필요'21.04.14.
-        thumb_urls(driver)
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while(True):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            new_height=driver.execute_script("return document.body.scrollHeight")
+            if(new_height==last_height):
+                break
+            last_height=new_height
+            thumb_urls(driver)
     else: # 익명로그인이 잘 안되었을때
         FL.fb_login(driver,is_login_FB,url)
-        thumb_urls(driver)
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while(True):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            new_height=driver.execute_script("return document.body.scrollHeight")
+            if(new_height==last_height):
+                break
+            last_height=new_height
+            thumb_urls(driver)
+        # print(all_thumbs) 모든 게시물 가져오는 것 확인완료
 
 def intro():
     print("<인스타그램 사진 다운로더>")
